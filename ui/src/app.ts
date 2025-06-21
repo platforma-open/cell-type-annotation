df1bb58c37cc35ae938e97935906793d41e7cedd
aiturbidemil <aiturbide@milaboratories.com>
2025-06-21 23:40:17 +0200

    improved ui

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,14 +1,12 @@
 import { model } from '@platforma-open/milaboratories.cell-type-annotation.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
 import MainPage from './pages/MainPage.vue';
-import UMAPPage from './pages/UMAP.vue';
 import tSNEPage from './pages/tSNE.vue';
 
 export const sdkPlugin = defineApp(model, () => {
   return {
     routes: {
       '/': () => MainPage,
-      '/umap': () => UMAPPage,
       '/tsne': () => tSNEPage,
     },
   };
